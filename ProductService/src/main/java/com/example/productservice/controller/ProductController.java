91a74bb2b9ee206ec0541f0f268adf08c5e1714e
Ibtid <ibtid011@example.com>
2022-02-26 19:08:51 +0600

    product order service done

---
+++ ProductService/src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.example.productservice.controller;
+
+
+
+
+import com.example.productservice.entity.Product;
+import com.example.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/")
+    public Product saveProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product findEmployeeById(@PathVariable("id") String id){
+        return productService.findProductById(id);
+    }
+}
